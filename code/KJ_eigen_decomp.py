0e8321854a35f456933e1524bf395635bf49ae74
Kaitlyn <kj22643@BME-MBP-A27511.local>
2019-06-21 14:58:41 -0500

    vary eigenvectors and apply to post treat data

--- code/KJ_eigen_decomp.py
+++ code/KJ_eigen_decomp.py
@@ -31,6 +31,7 @@ sc.settings.verbosity = 3
 #%% Load in pre and post data
 adata_pre = sc.read('KJ_adatapre.h5ad')
 adata_post = sc.read('KJ_adatapost.h5ad')
+# Make ann data objects into pandas data frames
 dfpre = pd.concat([adata_pre.obs['survivor'], adata_pre.obs['sample'],pd.DataFrame(adata_pre.raw.X,index=adata_pre.obs.index,
                             columns=adata_pre.var_names),],axis=1) 
 dfpost= pd.concat([adata_post.obs['sample'],pd.DataFrame(adata_post.raw.X,index=adata_post.obs.index,
@@ -42,6 +43,10 @@ sc.pl.umap(adata_pre,color=['survivor'],wspace=0.3,
 
 sc.pl.umap(adata_post,color=['sample'],wspace=0.3,
            save='post_treat_umap.png')
+
+#%% See if we cam plot the first two PCs
+sc.pl.pca(adata_pre, color=['survivor'],wspace=0.3,
+          save= 'pre_treat_pca.png')
 #%%
 print(dfpre) # 22192 columns corresponding to 22191 genes
 #%% Make series that label the pre-treatment cells as res/sens and label the 
@@ -49,6 +54,7 @@ print(dfpre) # 22192 columns corresponding to 22191 genes
 labelsdfpre = dfpre['survivor']
 print(labelsdfpre)
 samplabspost = dfpost['sample']
+print(samplabspost)
 #%% Make matrices (data frames) of just the cell-gene matrix for the pre treatment and 
 # post treatment samples
 genematpre1 = dfpre.loc[:, dfpre.columns !='survivor']
@@ -65,9 +71,10 @@ print(genematpre)
 
 # let's see if we can make that in python and call it Adf
 npost =genematpost.shape[0]
-ncells = genematpre.shape[0] # this gets the number of rows in the df (number of cells)
+npre = genematpre.shape[0] # this gets the number of rows in the df (number of cells)
 ntrain = 8*round(ncells/10)  # start by setting the number of training cells to 1/10th 
-#%% Make your full data frames
+ntest = npre-ntrain
+#%% Make your full data frames (including the testing and training data set from the pre-treatment time point)
 # Call these Adf
 AdfT= genematpre
 Adf = AdfT.T
@@ -128,14 +135,16 @@ from numpy import linalg as LA
 lambdas, Vsmall= LA.eig(smallcov)
 # returns the eigenvalues (lambdas) and eigenvectors that are normalized (Euclidean norms are 1)
 print(lambdas)
-#%%
-print(lambdas[2])
+
 #%% Want to sort the lambdas in descending order
 # arg sort naturally sorts in ascending order, so make it negative
 ind_arr = np.argsort(-abs(lambdas))
+
 #%% Now your ind_arr should start with the highest eigenvalue and work its way down
 # print the indices to check that the first is the highest lambda and the last is the lowest
 print(ind_arr) 
+ordered_lambdas = lambdas[ind_arr]
+print(ordered_lambdas)
 #%% Reorder your lambdas and eigenvectors (Vsmall)
 Vnew = Vsmall[ind_arr]
 #%% Now apply to the big system
@@ -149,12 +158,15 @@ Vnew = Vsmall[ind_arr]
 # ngenes x ntrain x ntrain x ntrain gives ngenes x ntrain
 Vbig = np.matmul(Xmat, Vnew)
 # These vectors of length ntrain are the eigenvectors, in order of importance (eigenvalue)
+# Vbig is now an ngenes by ntrain matrix of eigenvectors (each column =1 eigenvector)
 
 #%% Renormalize: Now that you have your ngenes x ntraining cells matrix of 
 #eigenvectors, make sure the big covariance matrix is normalized
 norms = LA.norm(Vbig, axis = 0)
 Vnorm = Vbig/norms # divides each column by its norm
 testnorms = LA.norm(Vnorm, axis = 0) # want to ensure these are all essentially 1
+# Vnorm is your normalized, ordered, eigenvectors. They are ordered by the value of the 
+# ordered eigenvalue 
 #%% Now you have the Eigenspace, defined by Vnorm, which contains normalized eigenvectors of
 # the length of the number of genes, in order of importance.
 
@@ -168,6 +180,7 @@ testnorms = LA.norm(Vnorm, axis = 0) # want to ensure these are all essentially 
 
 # Declare the number of eigenvectors we want to use from the Vnorm matrix 
 neigs = 100
+#eventually we will loop through the number of eigenvectors and assess the accuracy as afunction of neigs
 trainmat = np.zeros((neigs, ntrain))
 trainvec= np.zeros((neigs,1))
 
@@ -229,42 +242,158 @@ for i in range(ntest):
     distcol = dist[:,i]
     ind = np.argsort(distcol)
     ordered_inds[:,i] = ind
+
+    
 #%% Use the ordered ind to make sense/res matrix
     # Using k=1 nearest neighbors classifier. Need to figure out how to extend this 
+
 for i in range(ntest):
-    index = ordered_inds[1,i]
+    index = ordered_inds[0,i]
     lab_est[i] = trainlabels[int(index)]
     print(trainlabels[int(index)])
-#%% See how well you did With your testing data 
-print(trainlabels[int(index)])
-print(testlabels[1])
-ct_corr = 0
-for i in range(ntest):
-    if lab_est[i]==testlabels[i]:
-        ct_corr+=1
-#%% 
-print(trainlabels[1]=='sens')
-#%%
-ct_sens = 0
-for k in range(ntrain):
-    if trainlabels[k]=='sens':
-        ct_sens+=1
-for j in range(ntest):
-    if testlabels[j] == 'sens':
-        ct_sens+=1
-pct_sens = ct_sens/ncells
-print(pct_sens)
-# Accuracy
-Acc= ct_corr/ntest
+#%%  Make a data frame with your actual and predicted classes
+ df = pd.DataFrame({'actual class':testlabels,'predicted class':lab_est})
+
+cnf_matrix = pd.crosstab(df['predicted class'],df['actual class'])
+
+hm = sns.heatmap(cnf_matrix,annot=True,fmt='d',robust=True,
+            linewidths=0.1,linecolor='black')   
+#%% Calculate some metrics of accuracy from the confusion matrix
+TPR=cnf_matrix.iloc[0,0]/ (sum(cnf_matrix.iloc[:,0]))
+print(TPR)
+TNR = cnf_matrix.iloc[1,1]/(sum(cnf_matrix.iloc[:,1]))
+print(TNR)
+PPV = cnf_matrix.iloc[0,0]/sum(cnf_matrix.iloc[0,:])
+print(PPV)
+NPV = cnf_matrix.iloc[1,1]/sum(cnf_matrix.iloc[1,:])
+print(NPV)
+Acc = (cnf_matrix.iloc[0,0]+ cnf_matrix.iloc[1,1,])/ntest
 print(Acc)
+prevalance = sum(cnf_matrix.iloc[:,0])/ntest
+print(prevalance)
+#%% Vary the number of eigenvectors and then compare the accuracy metrics
+neigvec = [10, 50, 75, 100, 150, 200, 250,]
+TPRi=np.zeros((len(neigvec),1))
+TNRi=np.zeros((len(neigvec),1))
+PPVi=np.zeros((len(neigvec),1)) 
+NPVi=np.zeros((len(neigvec),1)) 
+Acci=np.zeros((len(neigvec),1))
 
- #%%       
-    testdist = np.linalg.norm(testvec-trainvec)
-print(trainlabels[10:20])
-#%% Apply the classifier to the cells from the post treatment samples
 
-Xpo = Apost.sub(m, axis=0) # we subtract from each column
-Xpost = Xpo.as_matrix()
+#%%
+for k in range(len(neigvec)):
+    neigi=neigvec[k] # set the number of eigenvectors
+    
+    trainmat = np.zeros((neigi, ntrain))
+   
+    trainvec= np.zeros((neigi,1))
+    
+# First make your Omat which contains the coordinates (the columns of each training image
+# and has the corresponding assignment (sens or res) ))
+    for i in range(ntrain):
+        Mcol = Xmat[:,i] # start right after the last training column in Amat 
+        McolT= Mcol.T # to get the first testing cell as a row
+        # 1xngene x ngene x ntrain = 1 x ntrain
+        b=np.matmul(McolT,Vnorm)
+
+        trainvec= b[:neigi]
+        trainmat[:,i]= trainvec
+        # We want to save all of these reduced coordinate spaces
+        #eigenmatrices.append(trainmat)
+    #Project the testing cells into eigenspace
+    testmat = np.zeros((neigi, ntest))
+    testvec = np.zeros((neigi,1))
+    for i in range(ntest):
+        Mcol = Xtest[:,i]
+        McolT = Mcol.T
+        b = np.matmul(McolT, Vnorm)
+        testvec = b[:neigi]
+        testmat[:,i] = testvec
+# Compare the coordinates of the testing set with the coordinates of the training set cells
+
+
+    dist = np.zeros((ntrain,ntest))
+    #for i in range(ntest):
+    for i in range(ntest):
+        testvec = testmat[:,i]
+        #for j in range(ntrain):
+        for j in range(ntrain):
+            trainvec = trainmat[:,j]
+        # testvec of length neigs and train vec of length neigs
+        # find Euclidean distance between the two vectors
+            dist_j = np.linalg.norm(testvec-trainvec)
+        # fill in your distance vector
+            dist[j, i]=dist_j
+    # Now you have a distance matrix where each column is a testing cell
+    # for each column, we want to output the indices of the training vector distance
+    # in order from least to greatest
+    lab_est = [None]*ntest
+    for i in range(ntest):
+        distcol = dist[:,i]
+        ind = np.argsort(distcol)
+        ordered_inds[:,i] = ind
+    # Use the ordered ind to make sense/res matrix
+    # Using k=1 nearest neighbors classifier. Need to figure out how to extend this 
+
+    for i in range(ntest):
+        index = ordered_inds[0,i]
+        lab_est[i] = trainlabels[int(index)]
+
+    dfi = pd.DataFrame({'actual class':testlabels,'predicted class':lab_est})
+
+    cnf_matrixi = pd.crosstab(dfi['predicted class'],dfi['actual class'])
+
+    hm = sns.heatmap(cnf_matrixi,annot=True,fmt='d',robust=True,
+            linewidths=0.1,linecolor='black')   
+# Calculate some metrics of accuracy from the confusion matrix
+    TPRi[k] = cnf_matrixi.iloc[0,0]/ (sum(cnf_matrixi.iloc[:,0]))
+
+    TNRi[k] = cnf_matrixi.iloc[1,1]/(sum(cnf_matrixi.iloc[:,1]))
+
+    PPVi[k] = cnf_matrixi.iloc[0,0]/sum(cnf_matrixi.iloc[0,:])
+
+    NPVi[k] = cnf_matrixi.iloc[1,1]/sum(cnf_matrixi.iloc[1,:])
+
+    Acci[k] = (cnf_matrixi.iloc[0,0]+ cnf_matrixi.iloc[1,1,])/ntest
+
+#%% 
+print(TPRi)
+print(cnf_matrixi)
+print(k)
+print(cnf_matrixi.iloc[0,0]/ (sum(cnf_matrixi.iloc[:,0])))
+
+#%% Plot the accuracy metrics versus number of eigenvectors
+import matplotlib.pyplot as plt
+
+
+plt.plot(neigvec, TPRi, 'b-', label='TPR')
+plt.plot(neigvec, TNRi, 'g-', label='TNR')
+plt.plot(neigvec, PPVi, 'r-', label='PPV')
+plt.plot(neigvec, NPVi, 'y-', label='NPV')
+plt.plot(neigvec, Acci, 'k-', label='Accuracy')
+plt.xlabel('Number of eigenvectors')
+plt.ylabel('Metrics of Accuracy')
+plt.legend()
+loc= 'lower right'
+
+
+#%% BIG GAP FOR Applying the classifier to the post treatment samples!
+# Here we are doing it to just the 107 Aziz sample (one of the very late post treatment samples). 
+# We will combine this with the other sample, and we should also do the 30 hour time point.
+    
+    
+    
+    
+    
+    
+ #%% Project the post treatment cells into eigenspace
+dfpost107=dfpost[dfpost['sample'].str.contains("Aziz")]
+mpost = Apost.mean(axis =1)
+print(npost)
+#%%
+neigs=100
+X107 = Apost.sub(mpost, axis=0) # we subtract from each column
+Xpost = X107.as_matrix()
 postmat = np.zeros((neigs, npost))
 postvec = np.zeros((neigs,1))
 for i in range(npost):
@@ -273,10 +402,14 @@ for i in range(npost):
     b = np.matmul(McolT, Vnorm)
     postvec = b[:neigs]
     postmat[:,i] = postvec
-#%%
-ordered_inds_post = np.zeros((ntrain, ntest))
-#%%
-distpost = np.zeros((ntrain,npost))
+
+#%% Compare the coordinates of the testing set with the coordinates of the training set cells
+# Now you have a test mat which has class labels corresponding to each column
+# For each column of your test mat, you want to find the k training mat columns it is closest to
+# make a matrix that stores the ordered indices with the top being the lowest and the bottom the highest
+# Euclidean distance
+ordered_inds = np.zeros((ntrain, npost))
+dist = np.zeros((ntrain,npost))
 #for i in range(ntest):
 for i in range(npost):
     postvec = postmat[:,i]
@@ -287,10 +420,35 @@ for i in range(npost):
         # find Euclidean distance between the two vectors
         dist_j = np.linalg.norm(postvec-trainvec)
         # fill in your distance vector
-        distpost[j, i]=dist_j
+        dist[j, i]=dist_j
 
+#%% Now you have a distance matrix where each column is a testing cell
+# for each column, we want to output the indices of the training vector distance
+# in order from least to greatest
 lab_est_post = [None]*npost
 for i in range(npost):
-    distcol = distpost[:,i]
+    distcol = dist[:,i]
     ind = np.argsort(distcol)
-    ordered_inds_post[:,i] = ind
+    ordered_inds[:,i] = ind
+
+    
+#%% Use the ordered ind to make sense/res matrix
+    # Using k=1 nearest neighbors classifier. Need to figure out how to extend this 
+
+for i in range(npost):
+    index = ordered_inds[0,i]
+    lab_est_post[i] = trainlabels[int(index)]
+#%% Now that you have lab_est post, quantify the proportion.
+ct_sens=0
+for i in range(npost):
+    if lab_est_post[i]=='sens':
+        ct_sens+=1
+
+phi_est=ct_sens/npost
+print(phi_est)
+phi_est0=1-prevalance
+print(phi_est0)
+    
+    
+    
+    
