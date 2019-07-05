fdca5e01fa4b5526586bb5e6b72915d03725eec2
Kaitlyn <kj22643@wireless-10-146-77-199.public.utexas.edu>
2019-07-05 15:01:22 -0500

    build classifier on pretreatment 231 and CLL data sets and project post-treatment samples

--- code/KJ_est_mu_CLL.py
+++ code/KJ_est_mu_CLL.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Created on Fri Jul  5 11:48:27 2019
+
+@author: kj22643
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
 Created on Wed Jul  3 12:22:02 2019
 
 @author: kj22643
@@ -62,53 +70,49 @@ sc.set_figure_params(dpi_save=300)
 sc.settings.verbosity = 3
 
 #%% Load in pre and post treatment 231 data
-adata = sc.read('daylin_anndata.h5ad')
+adata = sc.read('HD_tpo-fm_filtered_scran_cc-regressed_fcounts-4k_7.2.h5ad')
 adata.obs.head()
 # current samples:
-#BgL1K
-#30hr
-#Rel-1
-#Rel-2
+#TP0
+# FM1
+# FM7
 # We will change these to time points
+
+
 #%% Assign survivor category in adata.obs
-longTreatLins = adata.obs.loc[(adata.obs['sample'].isin(['Rel-1','Rel-2']))&(adata.obs.lineage!='nan'),'lineage'].unique().tolist()
+longTreatLins = adata.obs.loc[(adata.obs['sample'].isin(['FM1','FM7']))&(adata.obs.lineage!='nan'),'lineage'].unique().tolist()
 
 adata.obs.loc[adata.obs.lineage.isin(longTreatLins)==False,'survivor'] = 'sens'
 adata.obs.loc[adata.obs.lineage.isin(longTreatLins)==True,'survivor'] = 'res'
 
 # %%try to rename the samples by time point
 samps= adata.obs['sample'].unique()
+timepoint = np.array(['t=0d', 't=29d'])
 
-timepoint = np.array(['t=0hr', 't=30hr', 't=1344hr'])
+adata.obs.loc[adata.obs['sample']==samps[0], 'timepoint']='t=0d'
+adata.obs.loc[adata.obs['sample']==samps[1], 'timepoint']='t=29d'
+adata.obs.loc[adata.obs['sample']==samps[2], 'timepoint']='t=29d'
 
-adata.obs.loc[adata.obs['sample']==samps[0], 'timepoint']='t=0hr'
-adata.obs.loc[adata.obs['sample']==samps[1], 'timepoint']='t=30hr'
-adata.obs.loc[adata.obs['sample']==samps[2], 'timepoint']='t=1344hr'
-adata.obs.loc[adata.obs['sample']==samps[3], 'timepoint']='t=1344hr'
 
 print(adata.obs['timepoint'].unique())
 
 
 
 #%% Separately make dataframes for the pre-treatment, intermediate, and post treatment samples
-# t=0 hr (pre-treatment), 3182 pre treatment cells
+# t=0 hr (pre-treatment), 2003 pre treatment cells, 21317 genes measured
+
 # We want to keep the info about the lineage so we can potentially
 # use it to make evenly divided testing and training data sets
-adata_pre = adata[adata.obs['timepoint']=='t=0hr', :]
+adata_pre = adata[adata.obs['timepoint']=='t=0d', :]
 dfpre = pd.concat([adata_pre.obs['survivor'], adata_pre.obs['lineage'],
                pd.DataFrame(adata_pre.raw.X,index=adata_pre.obs.index,
                             columns=adata_pre.var_names),],axis=1) 
-# t = 30 hr (intermediate timepoint) 5169 int treatment cells
-adata_int = adata[adata.obs['timepoint']=='t=30hr', :]
-dfint = pd.concat([adata_int.obs['lineage'],
-                   pd.DataFrame(adata_int.raw.X, index=adata_int.obs.index, 
-                                columns = adata_int.var_names),], axis=1)
-
-# t=1344 hr (~roughly 8 weeks), 10332 post treatment cells
-adata_post = adata[adata.obs['timepoint']=='t=1344hr', :]
+# t = 8 weeks, 6972 post-treatment cells
+adata_post = adata[adata.obs['timepoint']=='t=29d', :]
 dfpost = pd.concat([adata_post.obs['lineage'],
-                    pd.DataFrame(adata_post.raw.X, index=adata_post.obs.index, 
-                                 columns = adata_post.var_names),],axis =1)
+                   pd.DataFrame(adata_post.raw.X, index=adata_post.obs.index, 
+                                columns = adata_post.var_names),], axis=1)
+
 
 #%% Use sklearn to do principle component analysis on the  entire pre-treatment sample
 #X = dfpre.loc[:, dfpre.columns !='survivor', dfpre.columns !='lineage']
@@ -178,9 +182,9 @@ clf = svm.SVC(kernel=basis, C=Copt)
 y_PCA = {}
 mu_PCA = {}
 sigmasq_PCA = {}
-y_SVM = {}
-mu_SVM = {}
-sigmasq_SVM = {}
+#y_SVM = {}
+#mu_SVM = {}
+#sigmasq_SVM = {}
 V_train = {}
 
 
@@ -204,21 +208,21 @@ for i in range(kCV):
     y_PCA[i] = knn.predict(pca.transform(X_test))
 # Compute the nearest neighbor accuracy on the embedded test set
     mu_PCA[i] = sum(y_PCA[i])/ folds_dict['ntest'][i]
-    sigamasq_PCA[i] = mu_PCA[i]*(1-mu_PCA[i])./folds_dict['ntest'][i]
+    sigmasq_PCA[i] = mu_PCA[i]*(1-mu_PCA[i])/folds_dict['ntest'][i]
     # SVM MODEL OUTPUTS FOR EACH FOLD
-    clf.fit(X_train, y_train)
-    y_SVM[i]= clf.predict(X_test)
-    mu_SVM[i] = sum(y_SVM[i])/folds_dict['ntest'][i]
-    sigmasq_SVM[i] = mu_SVM[i]*(1-mu_SVM[i])./folds_dict['ntest'][i]
+    #clf.fit(X_train, y_train)
+    #y_SVM[i]= clf.predict(X_test)
+    #mu_SVM[i] = sum(y_SVM[i])/folds_dict['ntest'][i]
+    #sigmasq_SVM[i] = mu_SVM[i]*(1-mu_SVM[i])/folds_dict['ntest'][i]
     
 #%%  Put into folds_dict
 folds_dict['V_train'] = V_train
 folds_dict['y_PCA']= y_PCA
 folds_dict['mu_PCA'] = mu_PCA
 folds_dict['sigmasq_PCA'] = sigmasq_PCA
-folds_dict['y_SVM'] = y_SVM
-folds_dict['mu_SVM'] = mu_SVM
-folds_dict['sigmasq_SVM'] = sigmasq_SVM
+#folds_dict['y_SVM'] = y_SVM
+#folds_dict['mu_SVM'] = mu_SVM
+#folds_dict['sigmasq_SVM'] = sigmasq_SVM
 
    
     
@@ -227,28 +231,27 @@ folds_dict['sigmasq_SVM'] = sigmasq_SVM
 df = pd.DataFrame()
 dfprevtest = pd.DataFrame(folds_dict['prevtest'], index=[0])
 dfmu_PCA= pd.DataFrame(folds_dict['mu_PCA'], index = [0])
-dfmu_SVM = pd.DataFrame(folds_dict['mu_SVM'], index = [0])
+#dfmu_SVM = pd.DataFrame(folds_dict['mu_SVM'], index = [0])
 
 npprevtest=np.array(dfprevtest)
 npmu_PCA = np.array(dfmu_PCA)
-npmu_SVM = np.array(dfmu_SVM)
+#npmu_SVM = np.array(dfmu_SVM)
 mu_pre = np.mean(npprevtest)
 mu_hat_PCA = np.mean(npmu_PCA)
-mu_hat_SVM = np.mean(npmu_SVM)
+#mu_hat_SVM = np.mean(npmu_SVM)
 ntest = folds_dict['ntest'][0]
 print(mu_hat_PCA)
-print(mu_hat_SVM)
+#print(mu_hat_SVM)
                  
 sigmasq_PCA =(1-mu_hat_PCA)*mu_hat_PCA/ntest
-sigmasq_SVM = (1-mu_hat_SVM)*mu_hat_SVM/ntest
+#sigmasq_SVM = (1-mu_hat_SVM)*mu_hat_SVM/ntest
 print(sigmasq_PCA)
-print(sigmasq_SVM)
+#print(sigmasq_SVM)
 
 
 #%% Next step, apply the models to the subsequent time points!
 
 # Make your cell gene matrices
-Xint = dfint.drop(columns= ['lineage'])
 Xpost = dfpost.drop(columns =['lineage'])
 #%%
 # Already defined PCA and SVM hyperparameters above
@@ -262,19 +265,13 @@ knn.fit(pca.transform(X), y)
     
 y_pre = knn.predict(pca.transform(X))
 mu_pre_PCA = sum(y_pre)/len(X)
-# Now that you have the PC model, apply it to intermediate and post treatment samples.
-y_int = knn.predict(pca.transform(Xint))
-mu_int_PCA= sum(y_int)/len(Xint)
-sigmasq_int_PCA = mu_int_PCA*(1-mu_int_PCA)/len(Xint)
 
-
-
+# Now that you have the PC model, apply it to post treatment samples.
 y_post = knn.predict(pca.transform(Xpost))
 mu_post_PCA= sum(y_post)/len(Xpost)
 sigmasq_post_PCA = mu_post_PCA*(1-mu_post_PCA)/len(Xpost)
 #%% Print the PCA estimate outputs
 print(mu_pre_PCA)
-print(mu_int_PCA)
 print(mu_post_PCA)
 #%% Project the intermediate and post treatment samples into the principal component space
 # This is mostly for visualiation purposes, but also to evaluate if the post-treatment 
@@ -284,7 +281,6 @@ print(mu_post_PCA)
 
 
 PCspre=pca.transform(X)
-PCsint = pca.transform(Xint)
 PCspost = pca.transform(Xpost)
 
 #%%
@@ -293,29 +289,23 @@ PC_df.reset_index(drop=True, inplace=True)
 PC_df['classlabel'] = y
 PC_df['time'] = 0
 
-PCintdf = pd.DataFrame(PCsint)
-PCintdf.reset_index(drop=True, inplace=True)
-PCintdf['classlabel'] = y_int
-PCintdf['time'] = 30
+
 
 PCpostdf = pd.DataFrame(PCspost)
 PCpostdf.reset_index(drop=True, inplace=True)
 PCpostdf['classlabel'] = y_post
-PCpostdf['time'] = 1344
+PCpostdf['time'] = 29
 
 
-PCall = pd.concat([PC_df,PCintdf, PCpostdf], axis=0)
+PCall = pd.concat([PC_df, PCpostdf], axis=0)
 
 
-
-# A
 #%%
 sns.set_style('white')
 fig = plt.figure(figsize=(6,6))
 ax=sns.scatterplot(PCall[0], PCall[1], hue = PCall['time'])
 #%%
 ax=sns.scatterplot(PC_df[0], PC_df[1])
-ax = sns.scatterplot(PCintdf[0],PCintdf[1])
 ax = sns.scatterplot(PCpostdf[0], PCpostdf[1])
 
 ax.set(xlabel ='PC1', ylabel ='PC2') 
@@ -334,19 +324,27 @@ Xr= np.asarray(PC_dfres[0])
 Yr=np.asarray(PC_dfres[1])
 Zr=np.asarray(PC_dfres[2])
 
+Xpost = np.asarray(PCpostdf[0])
+Ypost = np.asarray(PCpostdf[1])
+Zpost = np.asarray(PCpostdf[2])
 
-Xint= np.asarray(PCintdf[0])
-Yint=np.asarray(PCintdf[1])
-Zint=np.asarray(PCintdf[2])
+PCpostsens = PCpostdf[PCpostdf['classlabel']==0]
+PCpostres = PCpostdf[PCpostdf['classlabel']==1]
 
-Xpost= np.asarray(PCpostdf[0])
-Ypost=np.asarray(PCpostdf[1])
-Zpost=np.asarray(PCpostdf[2])
+Xposts= np.asarray(PCpostsens[0])
+Yposts=np.asarray(PCpostsens[1])
+Zposts=np.asarray(PCpostsens[2])
 
-res_cells = ax.scatter(Xr, Yr, Zr, c='b', marker='^', alpha = 1, label = 't=0 hr res')
-sens_cells = ax.scatter(Xs, Ys, Zs, c='r', marker='o', alpha = 0.3, label = 't= 0 hr sens')
-#int_cells = ax.scatter(Xint, Yint, Zint, c='g', marker = 'o', alpha = 0.2, label = 't=30 hr')
-post_cells = ax.scatter(Xpost, Ypost, Zpost, c='c', marker = 'o', alpha = 0.1, label = 't=1344 hr')
+Xpostr= np.asarray(PCpostres[0])
+Ypostr=np.asarray(PCpostres[1])
+Zpostr=np.asarray(PCpostres[2])
+
+res_cells = ax.scatter(Xr, Yr, Zr, c='b', marker='^', alpha = 1, label = 't=0d res')
+sens_cells = ax.scatter(Xs, Ys, Zs, c='r', marker='o', alpha = 0.3, label = 't= 0d sens')
+
+#post_cells = ax.scatter(Xpost, Ypost, Zpost, c='g', marker = 'o', alpha = 0.3, label = 't=29 d')
+post_sens_cells = ax.scatter(Xposts, Yposts, Zposts, c='k', marker = 'o', alpha = 0.9, label = 't=29d sens ')
+post_res_cells = ax.scatter(Xpostr, Ypostr, Zpostr, c='c', marker = '^', alpha = 0.1, label = 't=29d res ')
 
 ax.set_xlabel('PC1')
 ax.set_ylabel('PC2')
@@ -373,18 +371,25 @@ Yr=np.asarray(PC_dfres[2])
 Zr=np.asarray(PC_dfres[3])
 
 
-Xint= np.asarray(PCintdf[1])
-Yint=np.asarray(PCintdf[2])
-Zint=np.asarray(PCintdf[3])
-
 Xpost= np.asarray(PCpostdf[1])
 Ypost=np.asarray(PCpostdf[2])
 Zpost=np.asarray(PCpostdf[3])
 
-res_cells = ax.scatter(Xr, Yr, Zr, c='b', marker='^', alpha = 1, label = 't=0 hr res')
-sens_cells = ax.scatter(Xs, Ys, Zs, c='r', marker='o', alpha = 0.3, label = 't= 0 hr sens')
-#int_cells = ax.scatter(Xint, Yint, Zint, c='g', marker = 'o', alpha = 0.2, label = 't=30 hr')
-#post_cells = ax.scatter(Xpost, Ypost, Zpost, c='c', marker = 'o', alpha = 0.1, label = 't=1344 hr')
+
+Xposts= np.asarray(PCpostsens[1])
+Yposts=np.asarray(PCpostsens[2])
+Zposts=np.asarray(PCpostsens[3])
+
+Xpostr= np.asarray(PCpostres[1])
+Ypostr=np.asarray(PCpostres[2])
+Zpostr=np.asarray(PCpostres[3])
+
+res_cells = ax.scatter(Xr, Yr, Zr, c='b', marker='^', alpha = 1, label = 't=0d res')
+sens_cells = ax.scatter(Xs, Ys, Zs, c='r', marker='o', alpha = 0.3, label = 't= 0d sens')
+
+#post_cells = ax.scatter(Xpost, Ypost, Zpost, c='g', marker = 'o', alpha = 0.3, label = 't=29 d')
+post_sens_cells = ax.scatter(Xposts, Yposts, Zposts, c='k', marker = 'o', alpha = 0.9, label = 't=29d sens ')
+post_res_cells = ax.scatter(Xpostr, Ypostr, Zpostr, c='c', marker = '^', alpha = 0.1, label = 't=29d res ')
 
 ax.set_xlabel('PC2')
 ax.set_ylabel('PC3')
@@ -412,18 +417,27 @@ Yr=np.asarray(PC_dfres[2])
 Zr=np.asarray(PC_dfres[3])
 
 
-Xint= np.asarray(PCintdf[0])
-Yint=np.asarray(PCintdf[2])
-Zint=np.asarray(PCintdf[3])
+
 
 Xpost= np.asarray(PCpostdf[0])
 Ypost=np.asarray(PCpostdf[2])
 Zpost=np.asarray(PCpostdf[3])
 
-res_cells = ax.scatter(Xr, Yr, Zr, c='b', marker='^', alpha = 1, label = 't=0 hr res')
-sens_cells = ax.scatter(Xs, Ys, Zs, c='r', marker='o', alpha = 0.3, label = 't= 0 hr sens')
-#int_cells = ax.scatter(Xint, Yint, Zint, c='g', marker = 'o', alpha = 0.2, label = 't=30 hr')
-post_cells = ax.scatter(Xpost, Ypost, Zpost, c='c', marker = 'o', alpha = 0.1, label = 't=1344 hr')
+Xposts= np.asarray(PCpostsens[0])
+Yposts=np.asarray(PCpostsens[2])
+Zposts=np.asarray(PCpostsens[3])
+
+Xpostr= np.asarray(PCpostres[0])
+Ypostr=np.asarray(PCpostres[2])
+Zpostr=np.asarray(PCpostres[3])
+
+res_cells = ax.scatter(Xr, Yr, Zr, c='b', marker='^', alpha = 1, label = 't=0d res')
+sens_cells = ax.scatter(Xs, Ys, Zs, c='r', marker='o', alpha = 0.3, label = 't= 0d sens')
+
+#post_cells = ax.scatter(Xpost, Ypost, Zpost, c='g', marker = 'o', alpha = 0.3, label = 't=29 d')
+post_sens_cells = ax.scatter(Xposts, Yposts, Zposts, c='k', marker = 'o', alpha = 0.9, label = 't=29d sens ')
+post_res_cells = ax.scatter(Xpostr, Ypostr, Zpostr, c='c', marker = '^', alpha = 0.1, label = 't=29d res ')
+
 
 ax.set_xlabel('PC1')
 ax.set_ylabel('PC3')
@@ -451,18 +465,26 @@ Yr=np.asarray(PC_dfres[1])
 Zr=np.asarray(PC_dfres[3])
 
 
-Xint= np.asarray(PCintdf[0])
-Yint=np.asarray(PCintdf[1])
-Zint=np.asarray(PCintdf[3])
+
 
 Xpost= np.asarray(PCpostdf[0])
 Ypost=np.asarray(PCpostdf[1])
 Zpost=np.asarray(PCpostdf[3])
 
-res_cells = ax.scatter(Xr, Yr, Zr, c='b', marker='^', alpha = 1, label = 't=0 hr res')
-sens_cells = ax.scatter(Xs, Ys, Zs, c='r', marker='o', alpha = 0.3, label = 't= 0 hr sens')
-#int_cells = ax.scatter(Xint, Yint, Zint, c='g', marker = 'o', alpha = 0.2, label = 't=30 hr')
-post_cells = ax.scatter(Xpost, Ypost, Zpost, c='c', marker = 'o', alpha = 0.1, label = 't=1344 hr')
+Xposts= np.asarray(PCpostsens[0])
+Yposts=np.asarray(PCpostsens[1])
+Zposts=np.asarray(PCpostsens[3])
+
+Xpostr= np.asarray(PCpostres[0])
+Ypostr=np.asarray(PCpostres[1])
+Zpostr=np.asarray(PCpostres[3])
+
+res_cells = ax.scatter(Xr, Yr, Zr, c='b', marker='^', alpha = 1, label = 't=0d res')
+sens_cells = ax.scatter(Xs, Ys, Zs, c='r', marker='o', alpha = 0.3, label = 't= 0d sens')
+
+#post_cells = ax.scatter(Xpost, Ypost, Zpost, c='g', marker = 'o', alpha = 0.3, label = 't=29 d')
+post_sens_cells = ax.scatter(Xposts, Yposts, Zposts, c='k', marker = 'o', alpha = 0.9, label = 't=29d sens ')
+post_res_cells = ax.scatter(Xpostr, Ypostr, Zpostr, c='c', marker = '^', alpha = 0.1, label = 't=29d res ')
 
 ax.set_xlabel('PC1')
 ax.set_ylabel('PC2')
